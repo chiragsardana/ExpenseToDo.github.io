bd704569608b2fa1333dac006f1f26db5ab35851
Chirag Sardana <30770247+chiragsardana@users.noreply.github.com>
2022-09-08 11:16:09 +0530

    ExpenseToDo Backend

--- ExpenseToDo-Backend/src/main/java/com/sbk/MicroserviceBasedApplication/controller/ToDoExpenseController.java
+++ ExpenseToDo-Backend/src/main/java/com/sbk/MicroserviceBasedApplication/controller/ToDoExpenseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sbk.MicroserviceBasedApplication.model.ToDoExpense;
 import com.sbk.MicroserviceBasedApplication.service.ToDoExpenseService;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/ToDoExpense")
 public class ToDoExpenseController {
@@ -41,5 +42,14 @@ public class ToDoExpenseController {
     public ResponseEntity<ToDoExpense> getExpenseById(@PathVariable("id") Long id){
         // System.out.println("The Id we are getting is "+id);
         return new ResponseEntity<ToDoExpense>(toDoExpenseService.getToDoExpenseById(id), HttpStatus.OK);
+    }
+    @GetMapping("/deleteById/{id}")
+    public ResponseEntity<Boolean> deleteExpenseById(@PathVariable("id") Long id){
+        // System.out.println("The Id we are getting is "+id);
+        return new ResponseEntity<Boolean>(toDoExpenseService.deleteToDoExpenseById(id), HttpStatus.OK);
+    }
+    @PostMapping("/updateById/{id}")
+    public ResponseEntity<ToDoExpense> updateExpenseById(@PathVariable("id") Long id, @RequestBody ToDoExpense toDoExpense){
+        return new ResponseEntity<ToDoExpense>(toDoExpenseService.updateToDoExpenseById(id, toDoExpense), HttpStatus.OK);
     }
 }
